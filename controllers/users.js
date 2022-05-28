10632c07e80f38e2904789c9828049c37f4f3b63
kostiya <kostiya@users.noreply.github.com>
2022-05-29 00:36:20 +0300

    Implemented users at end of part 4.c

--- controllers/users.js
+++ controllers/users.js
@@ -28,7 +28,8 @@ userRouter.post('/', async (request, response) => {
 })
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('notes', { content: 1, date: 1 })
     response.json(users)
 })
 
