ec10a5fe4286684c775635354ef075bc90f2776c
kostiya <kostiya@users.noreply.github.com>
2022-05-28 23:31:14 +0300

    Implemented all user get and post + tests

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    userName: String,
+    username: String,
     name: String,
     passwordHash: String,
     notes: [
